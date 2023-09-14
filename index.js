f9d14df06629622fcd3928bfaeb618a8893a033f
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-09-14 12:11:31 -0600

    server host

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from "cors";
 import subscriberRoutes from "./routes/subscriberRoute.js";
-import StripeRoute from './routes/StripeRoute.js'
+import StripeRoute from "./routes/StripeRoute.js";
 
 //pass : mk0dsWGiINJqz77m
 
@@ -23,12 +23,12 @@ app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/product", productRoutes);
-app.use("/api/v1/subscriber", subscriberRoutes );
-app.use("/api/v1",StripeRoute)
+app.use("/api/v1/subscriber", subscriberRoutes);
+app.use("/api/v1", StripeRoute);
 
 //rest api
 app.get("/", (req, res) => {
-  res.send(`this is a mern stack project`);
+  res.send(`<h1>Wellcome to flux server</h1>`);
 });
 
 //server run
